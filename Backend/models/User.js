c5e9f903c2067c42548c71bb11646f76743ca013
Vikas <88672035+vikas-nirmal@users.noreply.github.com>
2021-09-05 14:35:56 +0530

    routes added

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,29 +1,29 @@
 const mongoose = require("mongoose");
-const { Schema } =  mongoose;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
 
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
 
   password: {
     type: String,
-    required: true
+    required: true,
   },
 
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('user', UserSchema);
+const User = mongoose.model("user", UserSchema);
 User.createIndexes();
-module.exports = User
+module.exports = User;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,36 +1,45 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
 // Create user using: Post "api/auth/createuser" log in not required
-router.post('/createuser', [
-    body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'password must be atleast 5 characters').isLength({ min: 5 })   
-] , async (req, res)=>{
-//Errors
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    //Errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    
-    // check if user is already exist or not
-    let user = await User.findOne({email: req.body.email})
 
-    if(user){
-      return res.status(400).json({error: 'Sorry a user with this email already exists'})
-    }
-    user = await User.create({
+    // check if user is already exist or not
+    try {
+      let user = await User.findOne({ email: req.body.email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      })
-      res.json({"Nice": "nice"})
-      
-      // .then(user => res.json(user)).catch(err=> {console.log(err)
-      //   res.json({error: 'Email is already registered'})})
+      });
+      res.json(user);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
+  }
+);
 
-})
-
-module.exports = router
+module.exports = router;
